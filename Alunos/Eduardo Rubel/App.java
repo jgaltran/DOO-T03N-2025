002eaf2460fcdf3dddbb897162c94d653a830d78
Eduardo-Rubel <ercanavarro@minha.fag.edu.br>
2025-02-18 20:10:24 -0300

    aula01

---
+++ Alunos/Eduardo Rubel/App.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("");
+        Scanner scan=new Scanner(System.in);
+        int ab = 4;
+        System.out.println("ab");
+        int a;
+        a = scan.nextInt();
+        int b;
+        b = ab+a;   
+        System.out.println(b);
+    }
+}
