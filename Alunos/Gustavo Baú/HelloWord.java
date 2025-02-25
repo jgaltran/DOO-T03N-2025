7bc980b189a282348ce41dbc7f894113e85b3c06
Gustavo-Bau <gbau185@gmail.com>
2025-02-25 20:02:39 -0300

    heloWord

---
+++ Alunos/Gustavo Baú/HelloWord.java
@@ -0,0 +1,12 @@
+public class HelloWord {
+    public static void main(String[] args) throws Exception {
+
+    }
+    public void HelloWord() {
+    System.out.println("Hello, World!");
+    }
+   
+
+}
+
+
