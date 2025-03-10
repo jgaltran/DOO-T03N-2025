a7b44b4c16349fd783c4c1bba5622166af2a8c4f
GabrielSchauren <gajschauren@minha.fag.edu.br>
2025-03-10 00:06:23 -0300

    criado o metodo Hellow World

---
+++ Alunos/Gabriel Alex Junior Schauren/hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    public static void main(String[] args) {
+       helloworld(); 
+    }
+
+    public static void helloworld() {
+        System.out.println("Hello World!");
+    }
+}
