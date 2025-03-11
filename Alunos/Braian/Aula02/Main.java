b0bd0dd6e781b5829e630644d5c9f8bf64f9073f
BRAIAN GASPAR DA ROSA <103344009+BraianGaspar@users.noreply.github.com>
2025-03-11 15:37:32 -0300

    Update Main.java

--- Alunos/Braian/Aula02/Main.java
+++ Alunos/Braian/Aula02/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int option;
+        int opcao = 0;
 
         while (opcao != 3) {
             System.out.println("Menu:");
