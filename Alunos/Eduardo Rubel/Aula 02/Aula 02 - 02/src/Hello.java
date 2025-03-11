a060197592d5d627f898c9549fd03216f1aa0fc2
Eduardo-Rubel <ercanavarro@minha.fag.edu.br>
2025-03-11 19:38:35 -0300

    Update Hello.java

--- Alunos/Eduardo Rubel/Aula 02/Aula 02 - 02/src/Hello.java
+++ Alunos/Eduardo Rubel/Aula 02/Aula 02 - 02/src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Ola Mundo");
+        System.out.println("Hola que Tal");
     }
 }
