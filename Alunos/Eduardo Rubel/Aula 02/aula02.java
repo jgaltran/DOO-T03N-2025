4d6c42bcf481512f3a6c3420985c55d5e1ab4c76
Eduardo-Rubel <ercanavarro@minha.fag.edu.br>
2025-03-11 19:18:09 -0300

    Aula 02

--- Alunos/Eduardo Rubel/Aula 02/aula02.java
+++ Alunos/Eduardo Rubel/Aula 02/aula02.java
@@ -7,21 +7,21 @@ public class CalculadoraPlantas {
 
         do {
             // Menu
-            System.out.println("Bem-vindo à Calculadora da Gabrielinha!");
-            System.out.println("[1] - Calcular Preço Total");
+            System.out.println("Bem-vindo a Calculadora da Gabrielinha!");
+            System.out.println("[1] - Calcular Preco Total");
             System.out.println("[2] - Calcular Troco");
             System.out.println("[3] - Sair");
-            System.out.print("Escolha uma opção: ");
+            System.out.print("Escolha uma opcao: ");
             opcao = scanner.nextInt();
 
             switch (opcao) {
                 case 1:
                     System.out.print("Digite a quantidade de plantas: ");
                     int quantidade = scanner.nextInt();
-                    System.out.print("Digite o preço unitário da planta: ");
+                    System.out.print("Digite o preco unitario da planta: ");
                     double precoUnitario = scanner.nextDouble();
                     double precoTotal = quantidade * precoUnitario;
-                    System.out.printf("O preço total é: R$ %.2f%n", precoTotal);
+                    System.out.printf("O preco total e: R$ %.2f%n", precoTotal);
                     break;
 
                 case 2: 
@@ -31,18 +31,18 @@ public class CalculadoraPlantas {
                     double valorCompra = scanner.nextDouble();
                     double troco = valorRecebido - valorCompra;
                     if (troco < 0) {
-                        System.out.println("O valor recebido é insuficiente para realizar a compra.");
+                        System.out.println("O valor recebido e insuficiente para realizar a compra.");
                     } else {
-                        System.out.printf("O troco a ser dado é: R$ %.2f%n", troco);
+                        System.out.printf("O troco a ser dado e: R$ %.2f%n", troco);
                     }
                     break;
 
                 case 3: 
-                    System.out.println("Saindo da calculadora. Até mais!");
+                    System.out.println("Saindo da calculadora");
                     break;
 
                 default:
-                    System.out.println("Opção inválida. Tente novamente.");
+                    System.out.println("Opcao invalida");
             }
             System.out.println(); 
         } while (opcao != 3);
