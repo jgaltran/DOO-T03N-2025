14dcdab479f909bdeb1a2791eba5c7e272c72203
Manufag1 <mlmunaretto@minha.fag.edu.br>
2025-03-18 20:48:40 -0300

    corrigido arquivos vazios

--- Alunos/Manuela/Aula03/Vendas.java
+++ Alunos/Manuela/Aula03/Vendas.java
@@ -2,4 +2,81 @@ package objetos03;
 
 public class Vendas {
 
-}
+	private int quant;
+
+	private double valoruni;
+
+	private double total;
+
+	private double desc;
+
+		 public int getQuant() {
+
+			return quant;
+
+		}
+
+		public void setQuant(int quant) {
+
+			this.quant = quant;
+
+		}
+
+		public double getValoruni() {
+
+			return valoruni;
+
+		}
+
+		public void setValoruni(double valoruni) {
+
+			this.valoruni = valoruni;
+
+		}
+
+		public double getTotal() {
+
+			return total;
+
+		}
+
+		public void setTotal(double total) {
+
+			this.total = total;
+
+		}
+
+		public double getDesc() {
+
+			return desc;
+
+		}
+
+		public void setDesc(double desc) {
+
+			this.desc = desc;
+
+		}
+
+		public Vendas(int quant, double valoruni, double total, double desc) {
+
+		        this.quant = quant;
+
+		        this.valoruni = valoruni;
+
+		        this.total = total;
+
+		        this.desc= desc;
+
+		    }
+
+		
+
+		
+
+		
+
+	}
+
+
+
