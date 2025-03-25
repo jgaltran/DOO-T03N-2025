b62124c8c10874e2ddcbd1408d775d7c6191880f
Joao Pedro Müller Wilges <jpedro01230@gmail.com>
2025-03-25 20:21:40 -0300

    Add Aula 4

--- Alunos/Joao_Wilges/Aula4/src/fag/main.java
+++ Alunos/Joao_Wilges/Aula4/src/fag/main.java
@@ -17,13 +17,18 @@ public class main {
             System.out.println("""
                            [1] - Calcular Preço Total + Cadastrar Venda
                            [2] - Calcular Troco    
-                           [3] - Ver Vendas  
-                           [4] - Sair""" //
+                           [3] - Analise de vendas  
+                           [4] - Ver Vendas
+                           [5] - Sair""" //
                     //
             );
             int opcao = scanner.nextInt();
             switch (opcao){
                 case 1 -> {
+                    System.out.print("Informe o número do mês: ");
+                    int data_mes = scanner.nextInt();
+                    System.out.print("Informe o dia: ");
+                    int data_dia = scanner.nextInt();
                     System.out.print("Informe a quantidade vendida:");
                     int qtd_venda = scanner.nextInt();
                     System.out.print("Informe o valor do item vendido:");
@@ -32,7 +37,7 @@ public class main {
                         percent_desconto = 5;
                     }
                     Float total_venda = calculaPrecoDesconto(qtd_venda, vlr_item,percent_desconto);
-                    vendas.add(new Venda(qtd_venda, total_venda, percent_desconto, data));
+                    vendas.add(new Venda(qtd_venda, total_venda, percent_desconto, data_mes, data_dia));
                     System.out.println("Total da compra: R$" + total_venda);
                 }
                 case 2 -> {
@@ -43,12 +48,31 @@ public class main {
                     System.out.println("Troco: R$" + calculaTroco(vlr_pago, vlr_total));
                 }
                 case 3 -> {
+                    System.out.println("** Gerencial Vendas **");
+                    System.out.println("Informe o mês de consulta: ");
+                    int data_consulta_mes = scanner.nextInt();
+                    System.out.println("Informe o dia de consulta: ");
+                    int data_consulta_dia = scanner.nextInt();
+                    float total_vendas = 0;
+                    for(Venda venda: vendas){
+                        if(venda.getDataDia() == data_consulta_dia && venda.getDataMes() == data_consulta_mes){
+                            total_vendas += venda.getValorVenda();
+                        }
+                    }
+                    System.out.println("Total de vendas no dia " +
+                                    data_consulta_dia +
+                                    " e mês: " +
+                                    data_consulta_mes +
+                                    " R$: "+
+                            total_vendas);
+                }
+                case 4 -> {
                     System.out.println("Vendas:");
                     for (Venda venda : vendas){
                         System.out.println(venda.toString());
                     }
                 }
-                case 4 -> {
+                case 5 -> {
                     System.out.println("Finalizando sistema...");
                     flag = false;
                 }
@@ -57,17 +81,12 @@ public class main {
         }
         scanner.close();
     }
-    
-    private static float calculaPrecoTotal(Integer quantidade_vendida, Float valor_item){
-        return Math.round((quantidade_vendida * valor_item) * 100)/100;
-    }
 
     private static float calculaPrecoDesconto(Integer quantidade_vendida, Float valor_item, Integer valor_desconto){
         float total = Math.round((quantidade_vendida * valor_item) * 100)/100;
         total = total - (total*valor_desconto/100);
         return total;
     }
-
     private static float calculaTroco(float valor_pago, float valor_total){
         return valor_pago - valor_total;
     }
