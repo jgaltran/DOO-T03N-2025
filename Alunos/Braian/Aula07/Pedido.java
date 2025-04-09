548d7c4c944858ba2fd8292801cceaef8647241c
Braian Gaspar Da Rosa <braian_gaspar@hotmail.com>
2025-04-09 17:58:32 -0300

    Arrumando as classes

--- Alunos/Braian/Aula07/Pedido.java
+++ Alunos/Braian/Aula07/Pedido.java
@@ -1,10 +1,5 @@
-package Alunos.Braian.Aula07;
-
-import java.util.Date;
-
-public class Pedido{
-  private String atributos;
-  private Integer id;
+public class Pedido {
+  private int id;
   private Date dataCriacao;
   private Date dataPagamento;
   private Date dataVencimentoReserva;
@@ -13,8 +8,7 @@ public class Pedido{
   private String loja;
   private Item[] itens;
 
-  public Pedido(String atributos, Integer id, Date dataCriacao, Date dataPagamento, Date dataVencimentoReserva, String cliente, String vendedor, String loja, Item[] itens){
-    this.atributos = atributos;
+  public Pedido(int id, Date dataCriacao, Date dataPagamento, Date dataVencimentoReserva, String cliente, String vendedor, String loja, Item[] itens) {
     this.id = id;
     this.dataCriacao = dataCriacao;
     this.dataPagamento = dataPagamento;
@@ -24,18 +18,22 @@ public class Pedido{
     this.loja = loja;
     this.itens = itens;
   }
-  public void calcularValorTotal(){
-    System.out.println("Pedido do cliente: " + cliente);
-    for(Item item : itens){
-      item.gerarDescricao();
-    }
-  }
-  public void gerarDescricaoVenda(){
+
+  public double calcularValorTotal() {
     double total = 0;
-    for (Item item : itens){
+    for (Item item : itens) {
       total += item.getValor();
     }
-    System.out.println("Valor total do pedido:" + total);
+    System.out.println("Valor total do pedido: R$" + total);
+    return total;
+  }
 
+  public void gerarDescricaoVenda() {
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    System.out.println("Pedido criado em: " + sdf.format(dataCriacao) + ", Valor total: R$" + calcularValorTotal());
+  }
+
+  public Date getDataVencimentoReserva() {
+    return dataVencimentoReserva;
   }
 }
