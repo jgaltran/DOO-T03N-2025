248ed9aee7041f78cc4fe0407ff61c113b47efb4
Braian Gaspar Da Rosa <braian_gaspar@hotmail.com>
2025-04-09 18:06:59 -0300

    Implementação herança básica

--- Alunos/Braian/Aula07/Pessoa.java
+++ Alunos/Braian/Aula07/Pessoa.java
@@ -1,25 +1,43 @@
 package Alunos.Braian.Aula07;
 
 public class Pessoa {
-
   protected String nome;
-  protected Integer idade;
+  protected int idade;
+  protected String loja;
 
-    public Pessoa(String nome, Integer idade){
+  public Pessoa(String nome, int idade, String loja) {
       this.nome = nome;
       this.idade = idade;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public Integer getIdade() {
+      this.loja = loja;
+  }
+
+  public void apresentarSe() {
+      System.out.println("Nome: " + nome);
+      System.out.println("Idade: " + idade);
+      System.out.println("Loja: " + loja);
+  }
+
+  public String getNome() {
+      return nome;
+  }
+
+  public void setNome(String nome) {
+      this.nome = nome;
+  }
+
+  public int getIdade() {
       return idade;
-    }
-    public void setNome(String nome) {
-      this.nome = nome;
-    }
-    public void setIdade(Integer idade) {
-      this.idade = idade;
-    }
+  }
 
+  public void setIdade(int idade) {
+      this.idade = idade;
+  }
+
+  public String getLoja() {
+      return loja;
+  }
+
+  public void setLoja(String loja) {
+      this.loja = loja;
+  }
 }
--- Alunos/Braian/Aula07/gerente.java
+++ Alunos/Braian/Aula07/gerente.java
@@ -1,40 +1,56 @@
 package Alunos.Braian.Aula07;
-public class Gerente {
-  private String nome;
-  private int idade;
-  private String loja;
+public class Gerente extends Pessoa{
   private double salarioBase;
-  private double[] salarioRecebido;
-  private Endereco endereco;
+    private double[] salarioRecebido;
+    private Endereco endereco;
 
-  public Gerente(String nome, int idade, String loja, double salarioBase, double[] salarioRecebido, Endereco endereco) {
-    this.nome = nome;
-    this.idade = idade;
-    this.loja = loja;
-    this.salarioBase = salarioBase;
-    this.salarioRecebido = salarioRecebido;
-    this.endereco = endereco;
-  }
-
-  public void apresentarse() {
-    System.out.println("Nome: " + nome);
-    System.out.println("Idade: " + idade);
-    System.out.println("Loja: " + loja);
-  }
-
-  public double calcularMedia() {
-    double soma = 0;
-    for (double salario : salarioRecebido) {
-      soma += salario;
+    public Gerente(String nome, int idade, String loja, double salarioBase, Endereco endereco) {
+        super(nome, idade, loja);
+        this.salarioBase = salarioBase;
+        this.endereco = endereco;
+        this.salarioRecebido = new double[] { 1780.00, 1810.00, 1850.00 };
     }
-    return soma / salarioRecebido.length;
-  }
+    @Override
+    public void apresentarSe() {
+        super.apresentarSe();
+        System.out.println("Salário Base: R$ " + salarioBase);
+        endereco.apresentarLogradouro();
+    }
 
-  public double calcularBonus() {
-    return salarioBase * 0.35;
-  }
+    public double calcularMedia() {
+        double soma = 0;
+        for (double salario : salarioRecebido) {
+            soma += salario;
+        }
+        return salarioRecebido.length > 0 ? soma / salarioRecebido.length : 0;
+    }
 
-  public void apresentarEndereco() {
-    endereco.apresentarLogradouro();
-  }
+    public double calcularBonus() {
+        return salarioBase * 0.35;
+    }
+
+    // Getters e setters (se desejar usar):
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public double[] getSalarioRecebido() {
+        return salarioRecebido;
+    }
+
+    public void setSalarioRecebido(double[] salarioRecebido) {
+        this.salarioRecebido = salarioRecebido;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
 }
