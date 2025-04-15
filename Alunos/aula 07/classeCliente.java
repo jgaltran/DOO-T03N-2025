1359cba26e89aa3d33812dfadb58492dce7e3ed4
WiniciusSantosLoebens <wsloebens@minha.fag.edu.br>
2025-04-15 20:03:35 -0300

    Add files via upload

--- Alunos/aula 07/classeCliente.java
+++ Alunos/aula 07/classeCliente.java
@@ -1,8 +1,8 @@
 import java.util.*;
 import java.text.SimpleDateFormat;
 
-class Vendedor extends Pessoa {
-    public Vendedor(String nome, int idade, Endereco endereco) {
+class Cliente extends Pessoa {
+    public Cliente(String nome, int idade, Endereco endereco) {
         super(nome, idade, endereco);
     }
 }
