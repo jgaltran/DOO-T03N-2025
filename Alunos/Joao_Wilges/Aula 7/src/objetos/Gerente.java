24d5a8f9d48ca0f1b0837064b6f3c2b4b1a635f8
Joao Pedro Müller Wilges <jpedro01230@gmail.com>
2025-04-15 23:11:36 -0300

    Ajustado Novas Classes de Pessoas - Fix Funcionario

--- Alunos/Joao_Wilges/Aula 7/src/objetos/Gerente.java
+++ Alunos/Joao_Wilges/Aula 7/src/objetos/Gerente.java
@@ -1,13 +1,9 @@
 package objetos;
 
-public class Gerente extends Pessoa{
+public class Gerente extends Funcionario{
 
     public Gerente(String nome, Loja loja, int idade, String cidade, String rua, String bairro, double salarioBase) {
         super(nome, loja, idade, cidade, rua, bairro, salarioBase);
-    }
-
-    public Gerente(String nome, double salarioBase) {
-        super(nome, salarioBase);
     }
 
     public double calcularBonus() {
--- Alunos/Joao_Wilges/Aula 7/src/objetos/Pessoa.java
+++ Alunos/Joao_Wilges/Aula 7/src/objetos/Pessoa.java
@@ -13,48 +13,19 @@ public abstract class Pessoa {
     private double salarioRecebido3 = 0;
     private int qtd_salario = 3;
     private int contador_salario = 0;
-
-    public Pessoa(String nome, Loja loja, int idade, String cidade, String rua, String bairro, double salarioBase) {
+    public Pessoa(String nome, int idade, String cidade, String rua, String bairro) {
         this.nome = nome;
-        this.loja = loja;
         this.idade = idade;
         this.cidade = cidade;
         this.rua = rua;
         this.bairro = bairro;
-        this.salarioBase = salarioBase;
-    }
-    public Pessoa(String nome, double salarioBase) {
-        this.nome = nome;
-        this.salarioBase = salarioBase;
     }
 
     public String getNome() {
         return this.nome;
     }
 
-    public void apresentarse() {
-        System.out.println("Nome: " + nome);
-        System.out.println("Idade: " + idade);
-        System.out.println("Loja: " + loja);
-    }
-    public double calcularMedia() {
-        return (salarioRecebido1 + salarioRecebido2 + salarioRecebido3) / qtd_salario;
-    }
-    public double calcularBonus(Float porcentagem_bonus){
-        return salarioBase * porcentagem_bonus;
-    }
-
-    public void adicionarSalarioRecebido(double salario){
-        switch (contador_salario){
-            case 0:
-                salarioRecebido1 = salario;
-                contador_salario ++;
-            case 1:
-                salarioRecebido2 = salario;
-                contador_salario ++;
-            case 2:
-                salarioRecebido3 = salario;
-                contador_salario = 0;
-        }
+    public int getIdade(){
+        return this.idade;
     }
 }
