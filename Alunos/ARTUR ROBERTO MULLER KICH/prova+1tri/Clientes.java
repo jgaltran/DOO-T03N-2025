03b1206e0e04067e763a901c6e55983be11086f0
Artur <arturkich6@gmail.com>
2025-04-22 20:05:45 -0300

    prova

---
+++ Alunos/ARTUR ROBERTO MULLER KICH/prova+1tri/Clientes.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Clientes {
+    String nome;
+    ArrayList<Livros> livros = new ArrayList<>();
+
+    public Clientes(String nome) {
+        this.nome = nome;
+    }
+}
