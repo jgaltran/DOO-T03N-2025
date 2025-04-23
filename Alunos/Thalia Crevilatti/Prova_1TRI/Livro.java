fe3b0c19eabdd448fc88bedbae3ee69997e9215c
Thalia Crevilatti <thaliacrevilatti44@gmail.com>
2025-04-22 21:09:12 -0300

    Prova 1º TRI

---
+++ Alunos/Thalia Crevilatti/Prova_1TRI/Livro.java
@@ -0,0 +1,35 @@
+public class Livro {
+    private String titulo;
+    private String autor;
+    private boolean emprestado;
+
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.emprestado = false;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public boolean isEmprestado() {
+        return emprestado;
+    }
+
+    public boolean podeSerEmprestado() {
+        return !emprestado;
+    }
+
+    public void emprestar() {
+        this.emprestado = true;
+    }
+
+    public void devolver() {
+        this.emprestado = false;
+    }
+}
